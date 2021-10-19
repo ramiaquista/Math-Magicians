2ba9c34c521a431703b543f488d94845ed686752
Ramiro Aquistapace <ramiaquista70@gmail.com>
2021-10-19 13:34:50 -0300

    Change App() by an App react component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
+import React from 'react';
 import Calculator from './components/Calculator';
-
-function App() {
-  return (
-    <main>
-      <Calculator />
-    </main>
-  );
+// eslint-disable-next-line react/prefer-stateless-function
+class App extends React.Component {
+  render() {
+    return (
+      <main>
+        <Calculator />
+      </main>
+    );
+  }
 }
 
 export default App;
