ed9c4c5c4e1c3501671f2ad47ae476a562ea607a
Ramiro Aquistapace <ramiaquista70@gmail.com>
2021-10-20 13:40:33 -0300

    Add obj state to Calculator React Component Import calculate() from ../logic/ Add onClickHandler() to call calculate() Add displayNum() to update dynamically the calculator display. Add onClick prop to all input tags.

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,40 +1,163 @@
 import React from 'react';
+import calculate from '../logic/calculate';
+// import operate from '../logic/operate';
 // eslint-disable-next-line react/prefer-stateless-function
 class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      obj: {
+        total: 0,
+        next: 0,
+        operation: '',
+      },
+    };
+  }
+
+  onClickHandler = (event) => {
+    const { obj } = this.state;
+    this.setState({
+      obj: calculate(obj, event.target.value),
+    });
+  };
+
+  displayNum = (object) => {
+    if (object.total === null || object.operation !== null) {
+      return object.next;
+    }
+    return object.total;
+  };
+
   render() {
+    const { obj } = this.state;
     return (
       <div className="calculator-container">
         <div className="res-display">
-          <input type="number" />
+          <input type="number" placeholder={this.displayNum(obj)} />
         </div>
         <div className="numpad-row">
-          <input className="normal-btn" type="button" value="AC" />
-          <input className="normal-btn" type="button" value="+/-" />
-          <input className="normal-btn" type="button" value="%" />
-          <input className="op-btns" type="button" value="÷" />
+          <input
+            className="normal-btn"
+            onClick={this.onClickHandler}
+            type="button"
+            value="AC"
+          />
+          <input
+            className="normal-btn"
+            onClick={this.onClickHandler}
+            type="button"
+            value="+/-"
+          />
+          <input
+            className="normal-btn"
+            onClick={this.onClickHandler}
+            type="button"
+            value="%"
+          />
+          <input
+            className="op-btns"
+            onClick={this.onClickHandler}
+            type="button"
+            value="÷"
+          />
         </div>
         <div className="numpad-row">
-          <input className="normal-btn" type="button" value="7" />
-          <input className="normal-btn" type="button" value="8" />
-          <input className="normal-btn" type="button" value="9" />
-          <input className="op-btns" type="button" value="x" />
+          <input
+            className="normal-btn"
+            onClick={this.onClickHandler}
+            type="button"
+            value="7"
+          />
+          <input
+            className="normal-btn"
+            onClick={this.onClickHandler}
+            type="button"
+            value="8"
+          />
+          <input
+            className="normal-btn"
+            onClick={this.onClickHandler}
+            type="button"
+            value="9"
+          />
+          <input
+            className="op-btns"
+            onClick={this.onClickHandler}
+            type="button"
+            value="x"
+          />
         </div>
         <div className="numpad-row">
-          <input className="normal-btn" type="button" value="4" />
-          <input className="normal-btn" type="button" value="5" />
-          <input className="normal-btn" type="button" value="6" />
-          <input className="op-btns" type="button" value="-" />
+          <input
+            className="normal-btn"
+            onClick={this.onClickHandler}
+            type="button"
+            value="4"
+          />
+          <input
+            className="normal-btn"
+            onClick={this.onClickHandler}
+            type="button"
+            value="5"
+          />
+          <input
+            className="normal-btn"
+            onClick={this.onClickHandler}
+            type="button"
+            value="6"
+          />
+          <input
+            className="op-btns"
+            onClick={this.onClickHandler}
+            type="button"
+            value="-"
+          />
         </div>
         <div className="numpad-row">
-          <input className="normal-btn" type="button" value="1" />
-          <input className="normal-btn" type="button" value="2" />
-          <input className="normal-btn" type="button" value="3" />
-          <input className="op-btns" type="button" value="+" />
+          <input
+            className="normal-btn"
+            onClick={this.onClickHandler}
+            type="button"
+            value="1"
+          />
+          <input
+            className="normal-btn"
+            onClick={this.onClickHandler}
+            type="button"
+            value="2"
+          />
+          <input
+            className="normal-btn"
+            onClick={this.onClickHandler}
+            type="button"
+            value="3"
+          />
+          <input
+            className="op-btns"
+            onClick={this.onClickHandler}
+            type="button"
+            value="+"
+          />
         </div>
         <div className="numpad-last-row">
-          <input className="normal-btn cero" type="button" value="0" />
-          <input className="normal-btn small" type="button" value="." />
-          <input className="op-btns small" type="button" value="=" />
+          <input
+            className="normal-btn cero"
+            onClick={this.onClickHandler}
+            type="button"
+            value="0"
+          />
+          <input
+            className="normal-btn small"
+            onClick={this.onClickHandler}
+            type="button"
+            value="."
+          />
+          <input
+            className="op-btns small"
+            onClick={this.onClickHandler}
+            type="button"
+            value="="
+          />
         </div>
       </div>
     );
